42ba1a490d46ba9051dcc43efa779f93db0e6af1
TeaChanathip <97622259+TeaChanathip@users.noreply.github.com>
2023-12-15 01:10:07 +0700

    DropDownMenu component
    Split the dropdown menu to a component

--- src/components/ConfirmWindow.vue
+++ src/components/ConfirmWindow.vue
@@ -39,7 +39,7 @@ export default {
     },
     methods: {
         closeWindow() {
-            this.$emit("close");
+            this.$emit("closeWindow");
         },
         doShake() {
             this.isShaking = true;
--- src/pages/Room.vue
+++ src/pages/Room.vue
@@ -14,20 +14,10 @@
         <CountDownTimer :timer="timer" />
     </div>
     <div class="dropdown-menu">
-        <img class="menu-icon" @click="toggleMenu()" src="/menu.svg" />
-        <div
-            :class="
-                isMenuShow
-                    ? 'menu-list menu-list-show'
-                    : 'menu-list menu-list-hide'
-            "
-        >
-            <button class="button-menu">How to Play?</button>
-            <button class="button-menu" @click="toggleWindow()">Leave</button>
-        </div>
+        <DropDownMenu @openWindow="toggleWindow()" />
     </div>
     <div>
-        <ConfirmWindow :isVisible="isWindowShow" @close="toggleWindow()" />
+        <ConfirmWindow :isVisible="isWindowShow" @closeWindow="toggleWindow()" />
     </div>
     <div class="player-list-bar-right">
         <RoomPlayerListBar
@@ -40,6 +30,7 @@
 <script>
 import CountDownTimer from "/src/components/CountDownTimer.vue";
 import RoomPlayerListBar from "/src/components/RoomPlayerListBar.vue";
+import DropDownMenu from "/src/components/DropDownMenu.vue"
 import ConfirmWindow from "/src/components/ConfirmWindow.vue";
 export default {
     props: {
@@ -52,6 +43,7 @@ export default {
         RoomPlayerListBar,
         CountDownTimer,
         ConfirmWindow,
+        DropDownMenu
     },
     data() {
         return {
@@ -72,7 +64,6 @@ export default {
             direction: 1,
             myCards: [],
 
-            isMenuShow: false,
             isWindowShow: false,
         };
     },
@@ -97,9 +88,6 @@ export default {
                         this.totalPlayer;
                 }
             }, this.playTime * 100);
-        },
-        toggleMenu() {
-            this.isMenuShow = !this.isMenuShow;
         },
         toggleWindow() {
             this.isWindowShow = !this.isWindowShow;
@@ -164,62 +152,4 @@ export default {
     right: 0%;
 }
 
-.menu-icon {
-    width: 3rem;
-    height: 3rem;
-    filter: invert(37%) sepia(90%) saturate(2023%) hue-rotate(240deg)
-        brightness(103%) contrast(104%);
-    cursor: pointer;
-
-    position: relative;
-    left: 100%;
-    transform: translate(calc(-100%));
-    padding-right: 1rem;
-    padding-bottom: 0.5rem;
-}
-
-.menu-icon:hover {
-    filter: invert(19%) sepia(66%) saturate(6926%) hue-rotate(262deg)
-        brightness(109%) contrast(110%);
-}
-
-.menu-list {
-    background: #4e4f50;
-    padding: 1rem;
-    border-top-left-radius: 8px;
-    border-bottom-left-radius: 8px;
-
-    display: flex;
-    flex-direction: row;
-    column-gap: 1rem;
-
-    position: fixed;
-    top: 12.5rem;
-    right: 0;
-
-    transition: transform 0.5s ease-out;
-}
-
-.menu-list-show {
-    transform: translate(0%);
-}
-
-.menu-list-hide {
-    transform: translate(100%);
-}
-
-.button-menu {
-    font-size: 1.5rem;
-    color: white;
-    background: #a35bff;
-    border: 0px;
-    border-radius: 8px;
-    width: 8rem;
-    height: 5rem;
-    cursor: pointer;
-}
-
-.button-menu:hover {
-    background: #8222ff;
-}
 </style>
